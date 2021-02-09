01a28a096239a6320cafa322f6fc198423bf5627
ishaanharry <ishaanharry@live.com>
2021-02-09 14:29:14 -0800

    okay, uncommented prize information

--- src/pages/outreach.js
+++ src/pages/outreach.js
@@ -47,13 +47,13 @@ const OutreachPage = () => (
         <br/> 
         2/13 (Sat) -- Deadline & Registration deadline   
         <br/> 
-    {/*<br/> 
+        <br/> 
         <b> Prizes: </b>
         <br/> 
         Participation -- T-shirt
         <br/>
         Winners -- Over $100 in prizes total
-        <br/>  */}
+        <br/>
         </p1>
         
         <div className="PandemicReliefButton">
